35092da8f95968aa2285522d3368627ad11b8a12
SbaoNaoki <bao.ki0420@gmail.com>
2023-12-06 15:06:54 +0900

    専攻選択機能、必修単位の判定機能を追加

--- src/Check.ts
+++ src/Check.ts
@@ -1,7 +1,37 @@
 import coins_media21 from './data/coins_media21.json';
+import coins_soft21 from './data/coins_soft21.json';
+import coins_sys21 from './data/coins_sys21.json';
+import Course from './Course';
 
-function Check() {
-    console.log(coins_media21);
+function Check(gradeslist: Course[], major: string) {
+    console.log(coins_soft21);
+
+    let requirement: any;
+
+    switch (major) {
+        case "coins_soft21":
+            requirement = coins_soft21;
+            break;
+        case "coins_media21":
+            requirement = coins_media21;
+            break;
+        case "coins_sys21":
+            requirement = coins_sys21;
+            break;
+        default:
+            requirement = coins_soft21;
+            break
+    }
+
+    let checklist: {[name: string]: boolean} = {};
+
+    let compulsory: string[] = requirement["courses"]["compulsory"];
+    compulsory.forEach((e) => {
+        checklist[e]=gradeslist.some((grades) => {
+                        return grades["name"] === e && !["D", "F", "履修中"].includes(grades["grade"]);
+                    });
+    });
+    console.log(checklist);
 }
 
 export default Check;
