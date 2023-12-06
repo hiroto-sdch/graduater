16cb1e29b5b260a49967c3fd92510b9dd93e2b38
SbaoNaoki <bao.ki0420@gmail.com>
2023-12-06 14:11:00 +0900

    CSVファイルの読み込み

---
+++ src/Course.ts
@@ -0,0 +1,9 @@
+interface Course {
+  id : string;
+  name : string;
+  unit : number;
+  grade : string;
+  year : number;
+}
+
+export default Course;
---
+++ src/Csvconvert.ts
@@ -0,0 +1,19 @@
+import Course from "./Course";
+
+function CSVconvert(csvdata: string) : Course[] {
+    csvdata = csvdata.replace(/"/g, "");
+    const csvlist : string[] = csvdata.split('\n');
+
+    return csvlist
+    .filter((e, i) => e && i !== 0)
+    .map((e) => {
+        const fields : string[] = e.split(",");
+        return {id: fields[2],
+                name: fields[3],
+                unit: Number(fields[4]),
+                grade: fields[7],
+                year: Number(fields[9])}
+    });
+}
+
+export default CSVconvert;
