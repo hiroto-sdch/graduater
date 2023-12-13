d643c2e634f0dad5f1f79f36e1aae909703f8b1b
SbaoNaoki <bao.ki0420@gmail.com>
2023-12-13 14:54:02 +0900

    いくつかのJSONファイルを追加

--- src/Check.ts
+++ src/Check.ts
@@ -1,6 +1,10 @@
 import coins_media21 from './data/coins_media21.json';
 import coins_soft21 from './data/coins_soft21.json';
 import coins_sys21 from './data/coins_sys21.json';
+import art_art21 from './data/art_art21.json';
+import art_japan21 from './data/art_japan21.json';
+import japanese21 from './data/japanese21.json';
+import pe21 from './data/pe21.json';
 import Course from './Course';
 import CheckCompulsory from './CheckCompulsory';
 import CheckSelect from './CheckSelect';
@@ -17,6 +21,18 @@ function Check(gradeslist: Course[], major: string) {
             break;
         case "coins_sys21":
             requirement = coins_sys21;
+            break;
+        case "art_art21":
+            requirement = art_art21;
+            break;
+        case "art_japan21":
+            requirement = art_japan21;
+            break;
+        case "japanese21":
+            requirement = japanese21;
+            break;
+        case "pe21":
+            requirement = pe21;
             break;
         default:
             requirement = coins_soft21;
