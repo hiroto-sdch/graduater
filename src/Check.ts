4555c5da1cd97ac0bbe99942648862611f3dacd9
SbaoNaoki <bao.ki0420@gmail.com>
2023-12-13 12:17:00 +0900

    "::"の入っている教科の判定機能を追加
    必修英語、体育、情報に対応

--- src/Check.ts
+++ src/Check.ts
@@ -3,6 +3,7 @@ import coins_soft21 from './data/coins_soft21.json';
 import coins_sys21 from './data/coins_sys21.json';
 import Course from './Course';
 import CheckSelect from './CheckSelect';
+import codeType from './data/courseCodeTypes';
 
 function Check(gradeslist: Course[], major: string) {
     console.log(coins_soft21);
@@ -21,7 +22,7 @@ function Check(gradeslist: Course[], major: string) {
             break;
         default:
             requirement = coins_soft21;
-            break
+            break;
     }
 
     let checklist: {[name: string]: boolean} = {};  // 科目名：履修したかどうか
@@ -32,7 +33,8 @@ function Check(gradeslist: Course[], major: string) {
             let hoge : (string)[][] = tmp.map((syn) => {
                 return syn.replace(/\[|\]|\s|\'/g,"").split(",");
             });
-            console.log(hoge);
+
+            // console.log(hoge);
             hoge.forEach((syn) => {
                 if (syn.every((_hoge) => (
                     gradeslist.some((grades) => 
@@ -40,9 +42,26 @@ function Check(gradeslist: Course[], major: string) {
                     )
                     )
                     )) {
-                    checklist[e]= true;
+                    checklist[e] = true;
+                }
+                else{
+                    checklist[e] = false;
                 }
             });
+
+        } else if (e.includes('::')) {
+            let tmp :string[] = e.split('::');
+            let codes :string[] = codeType[tmp[0] as keyof typeof codeType].codes;
+            let count :number = 0;
+       
+            codes.forEach((code) => {
+                gradeslist.forEach((grade) => {
+                    if (grade.id.startsWith(code) && !["D", "F", "履修中"].includes(grade.grade)){
+                        count += Number(grade.unit);
+                    }
+                });
+            });
+            checklist[tmp[0]] = (count >= Number(tmp[1]));
         }
         else {
             checklist[e]=gradeslist.some((grades) => {
