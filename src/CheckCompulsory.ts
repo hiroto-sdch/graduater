e5cd7e01b3a3aeb2abd7061ce1f45f01ec73f419
SbaoNaoki <bao.ki0420@gmail.com>
2023-12-13 14:27:08 +0900

    Check全体を動かすファイルを実装

--- src/CheckCompulsory.ts
+++ src/CheckCompulsory.ts
@@ -1,30 +1,11 @@
-import coins_media21 from './data/coins_media21.json';
-import coins_soft21 from './data/coins_soft21.json';
-import coins_sys21 from './data/coins_sys21.json';
 import Course from './Course';
-import CheckSelect from './CheckSelect';
-import CheckCompulsory from './CheckCompulsory';
+import codeType from './data/courseCodeTypes';
 
-function Check(gradeslist: Course[], major: string) {
-    // 卒業要件のJSONを格納
-    let requirement: any;
-    switch (major) {
-        case "coins_soft21":
-            requirement = coins_soft21;
-            break;
-        case "coins_media21":
-            requirement = coins_media21;
-            break;
-        case "coins_sys21":
-            requirement = coins_sys21;
-            break;
-        default:
-            requirement = coins_soft21;
-            break;
-    }
+function CheckCompulsory(gradeslist: Course[], requirement: any) {
 
     let checklist: {[name: string]: boolean} = {};  // 科目名：履修したかどうか
     let compulsory: string[] = requirement["courses"]["compulsory"];    // 必修単位の科目名
+    
     compulsory.forEach((e) => {
         if (e.includes('//')) {
             let tmp :string[] = e.split('//');
@@ -87,11 +68,6 @@ function Check(gradeslist: Course[], major: string) {
         }
     });
     
-    // console.log(checklist);
-    // console.log(failureunit);
-    // console.log(gradeslist);
-
-    CheckSelect(gradeslist, requirement);
 }
 
-export default Check;
+export default CheckCompulsory;
