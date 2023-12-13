647a56384a82f3d230661cf1691cb422a4e66211
SbaoNaoki <bao.ki0420@gmail.com>
2023-12-13 13:53:21 +0900

    必修科目のチェック済みフラグに対応

--- src/Check.ts
+++ src/Check.ts
@@ -2,7 +2,6 @@ import coins_media21 from './data/coins_media21.json';
 import coins_soft21 from './data/coins_soft21.json';
 import coins_sys21 from './data/coins_sys21.json';
 import Course from './Course';
-import CheckSelect from './CheckSelect';
 import codeType from './data/courseCodeTypes';
 
 function Check(gradeslist: Course[], major: string) {
@@ -34,18 +33,24 @@ function Check(gradeslist: Course[], major: string) {
                 return syn.replace(/\[|\]|\s|\'/g,"").split(",");
             });
 
-            // console.log(hoge);
-            hoge.forEach((syn) => {
+            hoge.some((syn) => {
+                syn.forEach((_hoge) => {
+                    gradeslist.forEach((grades) => {
+                        if (_hoge.includes(grades["name"])){
+                            grades.checked = true;
+                        }
+                    });
+                });
                 if (syn.every((_hoge) => (
                     gradeslist.some((grades) => 
-                    grades["name"] === _hoge && !["D", "F", "履修中"].includes(grades["grade"])
+                    grades["name"] === _hoge && !["D", "F"].includes(grades["grade"])
                     )
                     )
                     )) {
-                    checklist[e] = true;
+                    return checklist[e] = true;
                 }
                 else{
-                    checklist[e] = false;
+                    return checklist[e] = false;
                 }
             });
 
@@ -55,9 +60,10 @@ function Check(gradeslist: Course[], major: string) {
             let count :number = 0;
        
             codes.forEach((code) => {
-                gradeslist.forEach((grade) => {
-                    if (grade.id.startsWith(code) && !["D", "F", "履修中"].includes(grade.grade)){
-                        count += Number(grade.unit);
+                gradeslist.forEach((grades) => {
+                    if (grades.id.startsWith(code) && !["D", "F"].includes(grades.grade)){
+                        count += Number(grades.unit);
+                        grades.checked = true;
                     }
                 });
             });
@@ -65,14 +71,26 @@ function Check(gradeslist: Course[], major: string) {
         }
         else {
             checklist[e]=gradeslist.some((grades) => {
-                return grades["name"] === e && !["D", "F", "履修中"].includes(grades["grade"]);
+                return grades["name"] === e && !["D", "F"].includes(grades["grade"]);
             });
         }
     });
-    
-    console.log(checklist);
 
-    CheckSelect(gradeslist, requirement);
+    let failureunit: string[] = [];
+    Object.keys(checklist).forEach((key) => {
+        if (checklist[key] === false){
+            failureunit.push(key);
+        }
+    });
+    gradeslist.forEach((grades) => {
+        if (Object.keys(checklist).includes(grades["name"])){
+            grades.checked = true;
+        }
+    });
+    
+    // console.log(checklist);
+    // console.log(failureunit);
+    // console.log(gradeslist);
 }
 
 export default Check;
