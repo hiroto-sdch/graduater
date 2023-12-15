4ae9da9b395111dc18eda817819c40829f39c4fe
SbaoNaoki <bao.ki0420@gmail.com>
2023-12-15 18:47:00 +0900

    kdbのjsonファイルを追加

---
+++ src/Recommend.ts
@@ -0,0 +1,9 @@
+import codeType from "./data/courseCodeTypes";
+import Course from "./Course";
+import kdb from "./data/kdb.json"
+
+function Recommend() {
+    console.log(kdb)
+}
+
+export default Recommend;
