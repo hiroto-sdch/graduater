07f392cdb1fd305add3b2c4193b35a89a90bc56f
h-yki <yuuki531259@gmail.com>
2024-01-10 14:29:41 +0900

    細かい修正

--- src/CheckRecommendedExam.ts
+++ src/CheckRecommendedExam.ts
@@ -1,12 +1,12 @@
 import Course from "./Course";
 
 function CheckRecommendedExam(gradelist: Course[]){
-    let totalUnits = 0;
+    // let totalUnits = 0;
     let aGradeUnits = 0;
 
     gradelist.forEach((e) => {
         if (e.grade !== 'P') {
-            totalUnits += e.unit;
+            // totalUnits += e.unit;
             if (e.grade === 'A' || e.grade === 'A+') {
                 aGradeUnits += e.unit;
             }
